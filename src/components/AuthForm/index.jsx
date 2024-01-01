d1db14bbc5bb88e38214ed4bdfbfcb6af1ba8797
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-01-01 18:11:20 +0300

    Login in Sign up page UI

--- src/components/AuthForm/index.jsx
+++ src/components/AuthForm/index.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Image, Input, VStack } from '@chakra-ui/react'
+import { Box, Button, Flex, Image, Input, Text, VStack } from '@chakra-ui/react'
 import React, { useState } from 'react'
 
 function AuthForm() {
@@ -31,7 +31,28 @@ function AuthForm() {
                     {isLogin? "Log In" : "Sign Up"}
                 </Button>
 
+                <Flex alignItems={"center"} justifyContent={"center"} my={4} gap={1} w="full">
+                    <Box h={"1px"} backgroundColor={"gray.400"} flex={2}/>
+                    <Text mx={1} color='white'>OR</Text>
+                    <Box h={"1px"} backgroundColor={"gray.400"} flex={2}/>
+                </Flex>
+                <Flex alignItems={"center"} justifyContent={"center"} cursor="pointer">
+                    <Image src="/google.png" w={5} alt="Google logo"/>
+                    <Text mx={2} color={"blue.500"}>Log In with Google</Text>
+                </Flex>
+
             </VStack>
+        </Box>
+
+        <Box border="1px solid gray" borderRadius={4} padding={5} mt={4}>
+            <Flex alignItems={"center"} justifyContent={"center"}>
+                <Box mx={2} fontSize={14}>
+                    {isLogin? "Don't have an account?" : "Already have an account?"}
+                </Box>
+                <Box onClick={() => setIsLogin(!isLogin)} cursor={'pointer'} color="blue.500">
+                    {isLogin ? "Sign up" : "Log in"}
+                </Box>
+            </Flex>
         </Box>
     </>
   )
