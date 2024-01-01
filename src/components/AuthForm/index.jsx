6192c3bb54e726c97b02fd17f639603755683776
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-01-01 16:49:12 +0300

    Making the AuthForm component

--- src/components/AuthForm/index.jsx
+++ src/components/AuthForm/index.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import { Box, Button, Image, Input, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
 
 function AuthForm() {
+  const [isLogin, setIsLogin] = useState(true)
+
   return (
-    <div>AuthForm</div>
+    <>
+        <Box border="1px solid gray" borderRadius={4} padding={5}>
+            <VStack spacing={4}>
+                <Image src="/logo.png" h={24} cursor={"pointer"} alt="Instagram logo"/>
+                <Input 
+                    placeholder='Email'
+                    type="email"
+                    fontSize={14}
+                />
+                <Input 
+                    placeholder='Password'
+                    type="password"
+                    fontSize={14}
+                />
+                {!isLogin? (
+                    <Input 
+                        placeholder='Confirm    Password'
+                        type="password"
+                        fontSize={14}
+                    />
+                ) : null}
+
+                <Button w='full' colorScheme='blue' size="sm" fontSize={14}>
+                    {isLogin? "Log In" : "Sign Up"}
+                </Button>
+
+            </VStack>
+        </Box>
+    </>
   )
 }
 
