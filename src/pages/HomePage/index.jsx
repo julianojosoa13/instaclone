1dd9c6eb733461346af760d563baae01ca47ed27
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-01-03 12:17:45 +0300

    Feed Posts component

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,12 +1,24 @@
-import { Button } from '@chakra-ui/react'
+import { Box, Button, Container, Flex } from '@chakra-ui/react'
 import React from 'react'
+import FeedPosts from '../../components/FeedPosts'
 
 function HomePage() {
-  return (
-    <>
-        <div>HomePage</div>
-        <Button>Hello</Button>
-    </>
+  return (<Container maxW={"container.lg"}>
+    <Flex gap={20}>
+      <Box flex={2} py={10} border={"1px solid blue"}>
+        <FeedPosts />
+      </Box>
+      <Box 
+        flex={3} 
+        mr={20}
+        display={{base: "none", lg: "block"}}
+        maxW={"300px"}
+        border="1px solid red"
+      >
+        Suggested
+      </Box>
+    </Flex>
+  </Container>
   )
 }
 
