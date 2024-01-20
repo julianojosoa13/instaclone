a0bd52d1faaf077e5b2c342bc2db3255876c3cef
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-01-20 11:53:17 +0300

    Profile Header with Avatar and username

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,8 +1,36 @@
+import { Avatar, AvatarGroup, Flex, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 
 function ProfileHeader() {
   return (
-    <div>ProfileHeader</div>
+    <Flex
+        gap={{base: 4, sm: 10}}
+        py={10}
+        direction={{base: "column", sm: "row"}}
+    >
+        <AvatarGroup
+            size={{base: "xl", md: "2xl"}}
+            justifySelf={"center"}
+            alignSelf={"center"}
+            mx={"auto"}
+        >
+            <Avatar name="julianojosoa" src="/picofme.png" alt="user profile pic"/>
+        </AvatarGroup>
+
+        <VStack
+            alignItems={"start"}
+            gap={2}
+            mx={"auto"}
+            flex={1}
+        >
+            <Flex gap={4} direction={{base: "column", sm:"row"}} justifyContent={{base: "center", sm:"flex-start"}} alignItems={"center"} w="full"> 
+                <Text fontSize={{base: "sm", md:"lg"}}>
+                    julianojosoa
+                </Text>
+            </Flex>
+
+        </VStack>
+    </Flex>
   )
 }
 
--- src/pages/ProfilePage/index.jsx
+++ src/pages/ProfilePage/index.jsx
@@ -6,7 +6,7 @@ import ProfilePosts from '../../components/Profile/ProfilePosts'
 
 function ProfilePage() {
   return (
-    <Container maxW={"container-lg"} py={5}>
+    <Container maxW={"container.md"} py={5}>
         <Flex
             py={10}
             px={4}
@@ -17,7 +17,14 @@ function ProfilePage() {
         >
             <ProfileHeader />
         </Flex>
-        <Flex>
+        <Flex
+            px={{base: 2, sm: 4}}
+            maxW={"full"}
+            mx={"auto"}
+            borderTop={"1px solid"}
+            borderColor={"whiteAlpha.300"}
+            flexDirection={"column"}
+        >
             <ProfileTabs />
             <ProfilePosts />
         </Flex>
